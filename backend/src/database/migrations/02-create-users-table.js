363b132dd9326a46f3f77102f60b32f8a6ae2ee5
rafael moraes <rafaelsm003@gmail.com>
2022-11-15 04:18:13 -0300

    feat: cria migration para a tabela Users

---
+++ backend/src/database/migrations/02-create-users-table.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      accountId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+          model: 'Accounts',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Users');
+  },
+}
