8baf861b0382256357bb1d05a20482e9e6baa8b3
rafael moraes <rafaelsm003@gmail.com>
2022-11-15 08:20:04 -0300

    feat: faz com que a função registerTransactiopasse a receber dois objetos em seu parâmetro com o tipo das models User e Account

--- backend/src/utils/registerTransaction.ts
+++ backend/src/utils/registerTransaction.ts
@@ -1,4 +1,5 @@
-import { Sequelize } from 'sequelize';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ModelStatic, Sequelize } from 'sequelize';
 import User from '../database/models/user';
 import Account from '../database/models/account';
 import IUser from '../types/IUser';
@@ -7,17 +8,20 @@ import getPasswordHash from './getPasswordHash';
 import CustomError from '../types/CustomError';
 import StatusCodes from '../types/StatusCodes';
 
-const registerTransaction = async (userData: IUser) => {
+const registerTransaction = async (
+  userData: IUser,
+  accountModel: ModelStatic<Account>,
+  userModel: ModelStatic<User>,
+): Promise<User> => {
   const sequelize = new Sequelize(config);
   const { username, password } = userData;
   try {
     const result = await sequelize.transaction(async (t) => {
-      const { id } = await Account.create({ balance: 100 }, { transaction: t });
+      const { id } = await accountModel.create({ balance: 100 }, { transaction: t });
       const hashedPassword = getPasswordHash(password);
-      const newUser = await User.create({
+      const newUser = await userModel.create({
         username, password: hashedPassword, accountId: id,
       }, { transaction: t });
-
       return newUser;
     });
     return result;
