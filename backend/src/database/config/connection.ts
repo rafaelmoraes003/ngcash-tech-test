73dd541f2d8dc50e057247a86c03d15c9eff7ec9
rafael moraes <rafaelsm003@gmail.com>
2022-11-15 04:18:53 -0300

    feat: cria arquivo de configuração de dados do banco de dados

---
+++ backend/src/database/config/connection.ts
@@ -0,0 +1,17 @@
+import { Options } from 'sequelize';
+import 'dotenv/config';
+
+const config: Options = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '123456',
+  database: 'NG_CASH',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 3306,
+  dialect: 'mysql',
+  dialectOptions: {
+    timezone: 'Z',
+  },
+  logging: false,
+};
+
+module.exports = config;
