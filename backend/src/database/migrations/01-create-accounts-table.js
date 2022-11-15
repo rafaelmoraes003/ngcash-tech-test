3101d67bf82256ca6e25d8b66605c6882361a195
rafael moraes <rafaelsm003@gmail.com>
2022-11-15 04:18:00 -0300

    feat: cria migration para a tabela Accounts

---
+++ backend/src/database/migrations/01-create-accounts-table.js
@@ -0,0 +1,18 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Accounts', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      balance: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('Accounts');
+  },
+};
