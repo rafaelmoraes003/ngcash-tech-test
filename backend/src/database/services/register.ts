18543c180f7d441c974fcf6a35f857dc67a1af5a
rafael moraes <rafaelsm003@gmail.com>
2022-11-15 08:17:20 -0300

    feat: faz com que a classe RegisterService precise receber dois objetos do tipo User e Account nos parâmetros do constructor

--- backend/src/database/services/register.ts
+++ backend/src/database/services/register.ts
@@ -1,15 +1,30 @@
+import { ModelStatic } from 'sequelize';
 import IUser, { zodUserSchema } from '../../types/IUser';
 import validateBody from '../../utils/validateBody';
 import registerTransaction from '../../utils/registerTransaction';
 import StatusCodes from '../../types/StatusCodes';
 import validateUsername from '../../utils/validateUsername';
 import getToken from '../../utils/getToken';
+import User from '../models/user';
+import Account from '../models/account';
 
 class RegisterService {
-  public static async register(userData: IUser) {
+  private _userModel: ModelStatic<User>;
+  private _accountModel: ModelStatic<Account>;
+
+  constructor(userModel: ModelStatic<User>, accountModel: ModelStatic<Account>) {
+    this._userModel = userModel;
+    this._accountModel = accountModel;
+  }
+
+  public async register(userData: IUser) {
     validateBody(userData, zodUserSchema);
     await validateUsername(userData.username);
-    const newUser = await registerTransaction(userData);
+    const newUser = await registerTransaction(
+      userData,
+      this._accountModel,
+      this._userModel,
+    );
     const token = getToken(newUser.dataValues);
     return { code: StatusCodes.OK, data: { ...newUser.dataValues, token } };
   }
