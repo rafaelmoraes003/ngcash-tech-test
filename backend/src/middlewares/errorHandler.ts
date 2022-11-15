8d55cac4f8e2082629ff847a3cb982d4940fa7ed
rafael moraes <rafaelsm003@gmail.com>
2022-11-15 06:45:07 -0300

    feat: cria middleware de erro

---
+++ backend/src/middlewares/errorHandler.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import StatusCodes from '../types/StatusCodes';
+import CustomError from '../types/CustomError';
+
+const errorHandler = (
+  err: CustomError,
+  _req: Request,
+  res: Response,
+  _next: NextFunction,
+) => {
+  res
+    .status(err.statusCode ? err.statusCode : StatusCodes.SERVER_ERROR)
+    .json({ message: err.message });
+};
+
+export default errorHandler;
