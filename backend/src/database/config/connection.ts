eb0de31b215b4700ded5d86d81a23bbdf5fa55a5
rafael moraes <rafaelsm003@gmail.com>
2022-11-15 08:16:40 -0300

    feat: troca o dialeto do banco de dados para postgres

--- backend/src/database/config/connection.ts
+++ backend/src/database/config/connection.ts
@@ -6,8 +6,8 @@ const config: Options = {
   password: process.env.DB_PASSWORD || '123456',
   database: 'NG_CASH',
   host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  dialect: 'mysql',
+  port: Number(process.env.DB_PORT) || 5432,
+  dialect: 'postgres',
   dialectOptions: {
     timezone: 'Z',
   },
