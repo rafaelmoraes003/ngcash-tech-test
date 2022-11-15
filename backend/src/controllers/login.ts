e397e5eb5b6397a2d7683a7d82a3274e76a42b18
rafael moraes <rafaelsm003@gmail.com>
2022-11-15 08:14:43 -0300

    feat: cria o método login na classe LoginController para fazer login dos usuários

---
+++ backend/src/controllers/login.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Response, Request } from 'express';
+import LoginService from '../database/services/login';
+
+class LoginController {
+  private _loginService: LoginService;
+
+  constructor(loginService: LoginService) {
+    this._loginService = loginService;
+  }
+
+  public login = async (req: Request, res: Response, next: NextFunction) => {
+    const { username, password } = req.body;
+    try {
+      const { code, data } = await this._loginService.login({ username, password });
+      return res.status(code).json(data);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default LoginController;
