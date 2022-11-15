ed829fe9a6e16735af69128d294d6d0e90e8c562
rafael moraes <rafaelsm003@gmail.com>
2022-11-15 08:16:16 -0300

    feat: faz com que a classe RegisterController precise receber um objeto com tipo RegisterService como parâmetro do constructor

--- backend/src/controllers/register.ts
+++ backend/src/controllers/register.ts
@@ -2,10 +2,16 @@ import { NextFunction, Request, Response } from 'express';
 import RegisterService from '../database/services/register';
 
 class RegisterController {
-  public static register = async (req: Request, res: Response, next: NextFunction) => {
+  private _registerService: RegisterService;
+
+  constructor(registerService: RegisterService) {
+    this._registerService = registerService;
+  }
+
+  public register = async (req: Request, res: Response, next: NextFunction) => {
     const { username, password } = req.body;
     try {
-      const { code, data } = await RegisterService.register({ username, password });
+      const { code, data } = await this._registerService.register({ username, password });
       return res.status(code).json(data);
     } catch (error) {
       next(error);
