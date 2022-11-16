c218ee963019240e100e596926fd3246bd655f76
rafael moraes <rafaelsm003@gmail.com>
2022-11-16 01:46:03 -0300

    feat: implementa o endpoint GET /account

---
+++ backend/src/routes/account.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import auth from '../middlewares/auth';
+import AccountController from '../controllers/account';
+import Account from '../database/models/account';
+import AccountService from '../services/account';
+
+const account = Router();
+
+const accountService = new AccountService(Account);
+const accountController = new AccountController(accountService);
+
+account.get('/', auth, accountController.getById);
+
+export default account;
