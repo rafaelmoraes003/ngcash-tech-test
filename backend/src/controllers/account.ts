bbfd43ee84ef85fae57d0d9ef655b1d8d7e6c3dc
rafael moraes <rafaelsm003@gmail.com>
2022-11-16 01:44:03 -0300

    feat: cria o método getById na classe AccountController para buscar dados de uma conta com base em seu id

---
+++ backend/src/controllers/account.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Response } from 'express';
+import CustomRequest from '../types/CustomRequest';
+import AccountService from '../services/account';
+
+class AccountController {
+  private _accountService: AccountService;
+
+  constructor(accountService: AccountService) {
+    this._accountService = accountService;
+  }
+
+  public getById = async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const { userId } = req;
+    try {
+      const { code, data } = await this._accountService.getUserAccount(Number(userId));
+      return res.status(code).json(data);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
+
+export default AccountController;
