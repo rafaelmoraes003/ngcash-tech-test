7577d1599c308708143d0861a16261bed4ae4857
rafael moraes <rafaelsm003@gmail.com>
2022-11-16 01:46:50 -0300

    feat: cria o endpoint POST /transactions

---
+++ backend/src/routes/transactions.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import auth from '../middlewares/auth';
+import TransactionController from '../controllers/transactions';
+import TransactionService from '../services/transactions';
+import User from '../database/models/user';
+import Transaction from '../database/models/transaction';
+
+const transaction = Router();
+
+const transactionService = new TransactionService(User, Transaction);
+const transactionController = new TransactionController(transactionService);
+
+transaction.post('/', auth, transactionController.create);
+
+
+export default transaction;
