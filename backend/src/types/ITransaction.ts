98478e938e9ced9653ef991ecb33f425b77fa0c6
rafael moraes <rafaelsm003@gmail.com>
2022-11-17 03:13:44 -0300

    fix: faz com que value recebe numero maior que 0

--- backend/src/types/ITransaction.ts
+++ backend/src/types/ITransaction.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const zodTransactionSchema = z.object({
   creditedAccountUsername: z.string().min(3),
-  value: z.number().int(),
+  value: z.number().gt(0),
 });
 
 type ITransaction = z.infer<typeof zodTransactionSchema>;
